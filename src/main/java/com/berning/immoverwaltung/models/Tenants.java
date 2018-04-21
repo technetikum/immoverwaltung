cbfa196b0055914a5230a8e7c4f4695b640d2bb3
technetikum <technetikum-git@yahoo.com>
2018-04-21 21:03:06 +0200

    Added fields to tenants class

--- src/main/java/com/berning/immoverwaltung/models/Tenants.java
+++ src/main/java/com/berning/immoverwaltung/models/Tenants.java
@@ -1,5 +1,77 @@
 package com.berning.immoverwaltung.models;
 
 public class Tenants {
+    private String name;
+    private String forename;
+    private String street;
+    private String streetNumber;
+    private int postalCode;
+    private String city;
+    private String emailAddress;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
     
 }
